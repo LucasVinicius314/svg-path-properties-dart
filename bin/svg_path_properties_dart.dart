c2d239d555ac88d960795bc29007ba8141e03a95
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2023-07-07 23:53:51 -0300

    Initial commit

---
+++ bin/svg_path_properties_dart.dart
@@ -0,0 +1,6 @@
+import 'package:svg_path_properties_dart/svg_path_properties_dart.dart'
+    as svg_path_properties_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${svg_path_properties_dart.calculate()}!');
+}
---
+++ test/svg_path_properties_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:svg_path_properties_dart/svg_path_properties_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
